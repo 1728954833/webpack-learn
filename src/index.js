81a8920abc33b1c41f80a2bd03fb763b65e2ee49
1728954833 <1728954833@qq.com>
2021-09-17 20:42:39 +0800

    feat: 打包图片和字体

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import './index.less'
+import imgSrc from './bs_yk01d.png'
 
 const a = React.createElement('div')
 console.log(a)
+
+const img = document.createElement('img')
+img.src = imgSrc
+
+document.body.append(img)
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,18 @@ module.exports = {
         }, {
             test: /.less$/,
             use: ['style-loader', 'css-loader', 'less-loader']
+        }, {
+            test: /.(png|jpg|jpeg|gif)$/,
+            use: 'file-loader'
+        }, {
+            test: /.(woff|woff2|eot|ttf|otf)$/,
+            use: [{
+                loader: 'url-loader',
+                options: {
+                    // 小于这个大小打包成base64
+                    limit: 10240
+                }
+            }]
         }]
     }
 }
